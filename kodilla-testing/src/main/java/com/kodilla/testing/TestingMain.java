43308aaf6d3a54936f017e34c7f90426a052b829
Rafal-Kodilla <rjanus@wip.pl>
2017-08-01 22:17:08 +0200

    Moduł 6

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing;
+
+/**
+ * Created by RJanus on 01.08.2017.
+ */
+public class TestingMain {
+    public static void main (String[] args)
+    {
+        System.out.println("Moduł 6. Wprowadzenie do testów oprogramowania");
+    }
+}
