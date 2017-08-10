70dc4a0350bb1d1c36da79931c93630870a7434c
Rafal-Kodilla <rjanus@wip.pl>
2017-08-10 10:36:46 +0200

    Moduł 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,16 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.SaySomething;
 
 public class StreamMain {
     public static void main(String[] args) {
 
         Processor processor = new Processor();
-        Executor codeToExecute = () -> System.out.println("This is an example text.");
-        processor.execute(codeToExecute);
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor calculate = new ExpressionExecutor();
+        calculate.executeExpression(5.2,3, (a, b) -> a + b);
     }
 }
