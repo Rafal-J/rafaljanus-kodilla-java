96727349d3afb0c18efa444b233cb198f599acbd
Rafal-Kodilla <rjanus@wip.pl>
2017-08-12 11:00:14 +0200

    Moduł 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,39 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
+
+import java.util.ArrayList;
 
 
 public class StreamMain {
     public static void main(String[] args) {
+
+        String tekst = "8 3 -5 42 -1 0 0 -9 4 7 4 -4";
+        String number = "";
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for(int i = 0; i < tekst.length(); i++) {
+            if(tekst.substring(i,i+1).equals(" ")){
+                numbers.add(Integer.parseInt(number));
+                number = "";
+                continue;
+            }
+
+            number = number + tekst.charAt(i);
+        }
+
+        int max = numbers.get(0);
+        int min = max;
+
+        for(int i = 0; i < numbers.size(); i++){
+            max = (numbers.get(i) > max) ? numbers.get(i) : max;
+            min = (numbers.get(i) < min) ? numbers.get(i) : min;
+        }
+
+        System.out.println(max + " " + min);
+
+        String result = max + " " + min;
 
         PoemBeautifier poemToBeutify = new PoemBeautifier();
         String poem1 = "Ala ma kota.";
@@ -12,5 +41,9 @@ public class StreamMain {
         poemToBeutify.beautify(poem1, (poem2) -> poem2.toUpperCase());
         poemToBeutify.beautify(poem1, (poem2) -> poem2.replace("Ala","Zosia"));
         poemToBeutify.beautify(poem1, (poem2) -> poem2.substring(0,3));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(25);
+
     }
 }
