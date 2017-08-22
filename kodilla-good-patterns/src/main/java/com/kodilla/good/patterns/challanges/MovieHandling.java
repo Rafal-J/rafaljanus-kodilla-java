b73b0929014610be7f3a3f59de2c82a8a9867aa3
Rafal-Kodilla <rjanus@wip.pl>
2017-08-22 14:53:23 +0200

    Moduł 9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieHandling.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieHandling.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.challanges;
 import java.util.List;
 
 public class MovieHandling {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
     MovieStore moviesList = new MovieStore();
     moviesList.getMovies().stream()
             .flatMap(c -> c.stream())
