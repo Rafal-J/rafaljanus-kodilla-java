90a329bca34fd4fc00e085068d5c03427ba36272
Rafal-Kodilla <rjanus@wip.pl>
2017-08-22 14:31:39 +0200

    Moduł 9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/MovieStore.java
@@ -1,30 +1,15 @@
 package com.kodilla.good.patterns.challanges;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MovieStore {
 
     public ArrayList<List<String>> getMovies() {
 
-        List<String> ironManTranslations = new ArrayList<>();
-        ironManTranslations.add("Żelazny Człowiek");
-        ironManTranslations.add("Iron Man");
-
-        List<String> avengersTranslations = new ArrayList<>();
-        avengersTranslations.add("Mściciele");
-        avengersTranslations.add("Avengers");
-
-        List<String> flashTranslations = new ArrayList<>();
-        flashTranslations.add("Błyskawica");
-        flashTranslations.add("Flash");
-
         ArrayList<List<String>> moviesTitlesWithTranslations = new ArrayList<>();
-        moviesTitlesWithTranslations.add(ironManTranslations);
-        moviesTitlesWithTranslations.add(avengersTranslations);
-        moviesTitlesWithTranslations.add(flashTranslations);
+        moviesTitlesWithTranslations.add(new ArrayList<String>(Arrays.asList("Żelazny Człowiek","Iron Man")));
+        moviesTitlesWithTranslations.add(new ArrayList<String>(Arrays.asList("Mściciele","Avengers")));
+        moviesTitlesWithTranslations.add(new ArrayList<String>(Arrays.asList("Błyskawica","Flash")));
 
         return moviesTitlesWithTranslations;
     }
