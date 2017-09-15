120a6cd26f7778ae579926589111ea5fbfdb6ea7
Rafal-Kodilla <rjanus@wip.pl>
2017-09-15 14:01:15 +0200

    Moduł 12.5

---
+++ src/main/java/com/kodilla/codewars/stringToNumber/Application.java
@@ -0,0 +1,7 @@
+package com.kodilla.codewars.stringToNumber;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(StringToNumber.convertingToNumber("-4322"));
+    }
+}
