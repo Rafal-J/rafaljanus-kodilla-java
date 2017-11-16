b3e18d23d342691bfde283146dbdd2f838d968ef
Rafał Janus <RJanus@wip.pl>
2017-11-16 10:57:51 +0100

    Moduł 20.5

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/libraryb/Statistics.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/libraryb/Statistics.java
@@ -1,17 +1,25 @@
 package com.kodilla.patterns2.adapter.bookclassifier.libraryb;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Statistics implements  BookStatistics {
     @Override
     public int averagePublicationYear(Map<BookSignature, Book> books) {
         if (books.size() == 0) return 0;
-        int sum = 0;
-        for (Map.Entry<BookSignature, Book> entry : books.entrySet()) {
-            sum += entry.getValue().getYearOfPublication();
-        }
-        return sum / books.size();
+
+        List<Integer> yearsOfPublication = books.entrySet().stream()
+                .map(c -> c.getValue().getYearOfPublication())
+                .collect(Collectors.toList());
+
+        return IntStream.range(0,yearsOfPublication.size())
+                .map(c -> yearsOfPublication.get(c))
+                .sum() / yearsOfPublication.size();
     }
 
     @Override
